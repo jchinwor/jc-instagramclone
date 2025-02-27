3ff9d0b1680b89a98cbfb0aeb3a9b0ab014aa705
Jenkins Chinwor <90245242+jchinwor@users.noreply.github.com>
2025-02-27 14:59:36 +0000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
     // Force Babel to transpile the @faker-js/faker package
     transpileDependencies: ['@faker-js/faker'],
     chainWebpack: (config) => {
