617e6ccc8dba2afef1f8ba6006da6cf0a78c7347
Jenkins Chinwor <90245242+jchinwor@users.noreply.github.com>
2025-02-27 15:34:01 +0000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,14 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-     chainWebpack: config => {
-    config.module.rule('images')
+  
+    // Force Babel to transpile the @faker-js/faker package
+    transpileDependencies: ['@faker-js/faker'],
+    chainWebpack: (config) => {
+        config.module.rule('images')
       .use('file-loader')
       .tap(options => ({
         name: 'assets/[name].[hash:8].[ext]',
       }));
-  },
-    // Force Babel to transpile the @faker-js/faker package
-    transpileDependencies: ['@faker-js/faker'],
-    chainWebpack: (config) => {
       // Ensure Babel processes .mjs files in the faker package
       config.module
         .rule('js')
